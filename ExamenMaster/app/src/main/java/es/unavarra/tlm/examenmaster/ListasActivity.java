eab055a91ff21084c2f9758b8fe71d7ef89e672c
CarlosCalvoEizaguirre <carcaleiz@gmail.com>
2026-01-05 15:30:17 +0100

    Añado el tema de las listas y adaptadores de las mismas

--- ExamenMaster/app/src/main/java/es/unavarra/tlm/examenmaster/ListasActivity.java
+++ ExamenMaster/app/src/main/java/es/unavarra/tlm/examenmaster/ListasActivity.java
@@ -1,24 +1,44 @@
 package es.unavarra.tlm.examenmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import org.greenrobot.greendao.database.Database;
+import java.util.List;
 
 public class ListasActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private AccessDao accessDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_listas);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listView = findViewById(R.id.lv_Accesos);
+
+        // 1. INICIALIZAR DAO (Igual que en Login/DBActivity)
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "access-db");
+        Database db = helper.getWritableDb();
+        accessDao = new DaoMaster(db).newSession().getAccessDao();
+
+        // 2. CONSULTAR DATOS ORDENADOS DESCENDENTE
+        List<Access> lista = accessDao.queryBuilder()
+                .orderDesc(AccessDao.Properties.Created_at)
+                .list();
+
+        // 3. CONFIGURAR ADAPTADOR
+        AccessAdapter adapter = new AccessAdapter(this, lista);
+        listView.setAdapter(adapter);
+
+        // 4. EVENTO CLICK EN LA FILA
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            // El 'id' que recibimos es el que devolvió getItemId() en el adaptador
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("ACCESS_ID", id); // Pasamos solo el ID
+            startActivity(intent);
         });
     }
 }
